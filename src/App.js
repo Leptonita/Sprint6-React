10eb0a2575a2d2869f80f4c795072024062616bc
Glo <glo.movil@gmail.com>
2023-03-06 20:27:20 +0100

    SP6-ex1 Primera linea del componente Escena

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Escena from './components/Escena/Escena';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Escena />
     </div>
   );
 }
---
+++ src/components/Escena/Escena.js
@@ -0,0 +1,8 @@
+export default function Escena() {
+
+    return (
+        <div>
+            El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial
+        </div>
+    )
+}
